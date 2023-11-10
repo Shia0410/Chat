1d67560fc87ef7eca2614d99a1ea8ebd598257a7
Shia0410 <michael.shea.sauerman@gmail.com>
2023-11-10 17:24:07 +0200

    Added Changes

---
+++ Backend/API/API/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService; // Inject your user service here
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserLoginModel model)
+        {
+            // Implement login logic
+            var user = _userService.Login(model._username, model._password);
+
+            if (user == null)
+            {
+                // Return appropriate HTTP status code for login failure
+                return BadRequest("Invalid credentials");
+            }
+
+            // Return appropriate HTTP status code and user data
+            return Ok(new { Username = user.Username, /* other user data */ });
+        }
+    }
+
+}
+
---
+++ Backend/API/API/Models/UserLoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace API.Models
+{
+	public class UserLoginModel
+	{
+		public string _username;
+		public string _password;
+		public UserLoginModel(string Username, string Password)
+		{
+			_username = Username;
+			_password = Password;
+		}
+	}
+}
+
